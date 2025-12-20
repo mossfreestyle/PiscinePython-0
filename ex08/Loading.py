28cdf1c160a3b860857cd3f77e56a2ecf7381340
mossfreestyle <mosswassims3@gmail.com>
2025-12-20 16:37:40 +0100

    ex08

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,4 +1,23 @@
 import sys
 import os
+from typing import Generator, Iterable, Any
 
 
+def ft_tdqm(lst: range) -> Generator[Any, None, None]:
+    total = len(lst)
+    try:
+        bar_len = max(20, os.get_terminal_size().columns - 42)
+    except (OSError, AttributeError):
+        bar_len = 50
+
+    for i, elem in enumerate(lst, 1):
+        percent = int(100 * i / total)
+        filled = int(bar_len * i /total)
+        if filled > 0:
+            bar = '=' * (filled - 1) + '>'
+        else:
+            bar = ' '
+        bar = bar.ljust(bar_len, '=')
+        sys.stdout.write(f"\r{percent:3d}%|[{bar}]| {i}/{total}")
+        sys.stdout.flush()
+        yield elem
